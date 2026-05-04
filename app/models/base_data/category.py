2377bf0f4ba083a90b03547b2216df00ab73ccf6
DESKTOP-3AM7EOG\admin <2823921383@qq.com>
2026-05-05 02:12:45 +0800

    feat: 修改分类表中的字段注释，新增字段：category_type
    修改文件：
    - app/models/base_data/category.py

--- app/models/base_data/category.py
+++ app/models/base_data/category.py
@@ -11,10 +11,11 @@ class Category(BaseModel):
     )
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(50), nullable=False, comment='分类名称，如：上衣、裤子、裙子、外套')
+    name = db.Column(db.String(50), nullable=False, comment='分类名称')
     parent_id = db.Column(db.Integer, default=0, comment='父分类ID，0为顶级分类')
-    code = db.Column(db.String(50), nullable=False, comment='分类编码，如：TOP、PANTS、SKIRT')
+    code = db.Column(db.String(50), nullable=False, comment='分类编码')
     factory_id = db.Column(db.Integer, default=0, comment='所属工厂ID，0为全局默认，大于0为工厂自定义')
+    category_type = db.Column(db.String(20), default='style', comment='分类类型：style-款号，material-物料，order-订单')
     sort_order = db.Column(db.Integer, default=0, comment='排序，数字越小越靠前')
     status = db.Column(db.SmallInteger, default=1, comment='状态：1-启用，0-禁用')
     is_deleted = db.Column(db.SmallInteger, default=0, comment='逻辑删除：0-未删除，1-已删除')
